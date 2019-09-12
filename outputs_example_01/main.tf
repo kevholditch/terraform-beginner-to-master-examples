67ee6118d15628fe359861f49864add3a1153be2
Kevin Holditch <kevholditch@hotmail.com>
2019-09-12 20:36:03 +0100

    adding outputs example

---
+++ outputs_example_01/main.tf
@@ -0,0 +1,6 @@
+
+output "message" {
+  value = "Hello World"
+}
+
+
---
+++ outputs_example_02/main.tf
@@ -0,0 +1,20 @@
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "first_bucket" {
+  bucket = "kevholditch-bucket-outputs"
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.first_bucket.id
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.first_bucket.arn
+}
+
+output "bucket_information" {
+  value = "bucket name: ${aws_s3_bucket.first_bucket.id}, bucket arn: ${aws_s3_bucket.first_bucket.arn}"
+}
