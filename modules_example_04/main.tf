49b14c15b44774781862c95fcbbbc028fc3a055e
Kevin Holditch <kevholditch@hotmail.com>
2019-10-07 21:47:14 +0100

    adding extra module example

---
+++ modules_example_04/main.tf
@@ -0,0 +1,19 @@
+
+provider "aws" {
+  region  = "eu-west-1"
+  version = "~> 2.27"
+}
+
+module "work_queue" {
+  source      = "github.com/kevholditch/sqs-with-backoff"
+  queue_name  = "work-queue"
+}
+
+output "work_queue" {
+  value = module.work_queue.queue
+}
+
+output "work_queue_dead_letter_queue" {
+  value = module.work_queue.dead_letter_queue
+}
+
