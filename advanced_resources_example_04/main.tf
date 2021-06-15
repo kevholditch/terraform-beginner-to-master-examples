ab5c0c9d18fd279143c5997af9cc41cd35dcacc8
Kevin Holditch <kevholditch@hotmail.com>
2021-06-15 12:45:24 +0100

    updating the example

--- advanced_resources_example_04/main.tf
+++ advanced_resources_example_04/main.tf
@@ -1,5 +1,12 @@
-
 provider "aws" {
   region  = "eu-west-1"
   version = "~> 2.27"
+}
+
+resource "aws_sqs_queue" "queue" {
+    name = "queue"
+
+    lifecycle {
+      prevent_destroy = true
+    }
 }
